663e17501ba1a0fce553d5c10da4b1e6478a86f0
Paweł Rapkiewicz <pawel.rapkiewicz@gmail.com>
2022-04-29 17:33:13 +0000

    Fix for not exporting NutList object

--- asyncio_nutclient/__init__.py
+++ asyncio_nutclient/__init__.py
@@ -1,8 +1,8 @@
-from .nutclient import (UpsStatus, NutClient)
+from .nutclient import (UpsStatus, NutClient, NutList)
 from .version import __version__
 
 __author__ = 'Pawel Rapkiewicz'
 
 __email__ = 'pawel.rapkiewicz@gmail.com'
 
-__all__ = ['UpsStatus', 'NutClient', '__version__']
+__all__ = ['UpsStatus', 'NutClient', 'NutList', '__version__']
