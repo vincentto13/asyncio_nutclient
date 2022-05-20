e9771b49e429624a84390552d9e464121df53152
Paweł Rapkiewicz <pawel.rapkiewicz@gmail.com>
2022-05-20 13:51:40 +0000

    Few new things
    * UpsStatus is now handled as a list
    * UpsStatus is now extended with Charging, and discharging
    * UpsInstance type added
    * UpsVariableType type added
    * values are now reported without " and ' signs

--- asyncio_nutclient/__init__.py
+++ asyncio_nutclient/__init__.py
@@ -1,8 +1,8 @@
-from .nutclient import (UpsStatus, NutClient, NutList)
+from .nutclient import (UpsVariableType, UpsStatus, UpsInstance, NutClient, NutList)
 from .version import __version__
 
 __author__ = 'Pawel Rapkiewicz'
 
 __email__ = 'pawel.rapkiewicz@gmail.com'
 
-__all__ = ['UpsStatus', 'NutClient', 'NutList', '__version__']
+__all__ = ['UpsVariableType', 'UpsStatus', 'UpsInstance', 'NutClient', 'NutList', '__version__']
--- asyncio_nutclient/nutclient.py
+++ asyncio_nutclient/nutclient.py
@@ -1,6 +1,7 @@
 import asyncio
 import re
 from enum import Enum
+from typing import List
 import logging
 
 
@@ -12,13 +13,61 @@ def smart_split(text):
     return re.split("""\s+(?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
 
 
+class UpsVariableType(Enum):
+    Number = "NUMBER"
+    RW = "RW"
+    String = "STRING"
+    Enum = "ENUM"
+    Range = "RANGE"
+
+
 class UpsStatus(Enum):
     OnLine = "OL"
     OnBattery = "OB"
     LowBattery = "LB"
     ReplaceBattery = "RB"
+    Discharge = "DISCHRG"
+    Charge = "CHRG"
     CAL = "CAL"
     FSD = "FSD"
+
+
+class UpsInstance:
+    def __init__(self, ups: str, host: str, port: int) -> None:
+        self.ups = ups
+        self.host = host
+        self.port = port
+
+    def __hash__(self):
+        return hash((self.ups, self.host, self.port))
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return (
+            self.ups == other.ups
+            and self.host == other.host
+            and self.port == other.port
+        )
+
+    def __repr__(self):
+        return f'UpsInstance("{self.to_address()}")'
+
+    def to_address(self) -> str:
+        return f"{self.ups}@{self.host}:{self.port}"
+
+    def get_host_port(self):
+        return (self.host, self.port)
+
+    @classmethod
+    def from_address(cls, address):
+        m = re.findall(r"(\w+)\@([a-zA-Z0-9_.]+)\:(\d+)", address)
+        if not m or not len(m) == 1:
+            return None
+        data = m[0]
+        if not len(data) == 3:
+            return None
+        return cls(data[0], data[1], int(data[2]))
 
 
 class NutList(dict):
@@ -40,6 +89,10 @@ class NutClient:
         self.host = host
         self.port = port
 
+    @property
+    def connected(self):
+        return (self.reader is not None) and (self.writer is not None) and not self.writer.is_closing()
+
     async def connect(self):
         (self.reader, self.writer) = await asyncio.wait_for(
             asyncio.open_connection(self.host, self.port), timeout=5
@@ -53,7 +106,7 @@ class NutClient:
                 await self.writer.drain()
                 while True:
                     data = await asyncio.wait_for(self.reader.readline(), timeout=2)
-                    if not data:
+                    if not data and not self.connected():
                         await self.connect()
                         break
                     data = smart_split(data.decode("utf-8").strip())
@@ -105,7 +158,7 @@ class NutClient:
                 item_signature = self.command[1:]
                 if tuple(data[: len(item_signature)]) == item_signature:
                     item = data[len(item_signature) :]
-                    self.response[item[0]] = item[1]
+                    self.response[item[0]] = item[1].strip("\"'")
             return None
 
     class GenericGetCommandHandler(ErrorCommandHandler):
@@ -116,7 +169,7 @@ class NutClient:
         def parse(self, data):
             self.parse_error(data)
             if tuple(data[: len(self.command)]) == self.command:
-                return data[len(self.command) :]
+                return [item.strip("\"'") for item in data[len(self.command) :]]
 
     async def list_ups(self):
         command = ["LIST", "UPS"]
@@ -146,8 +199,11 @@ class NutClient:
         command = ["GET", cmd.upper(), *args]
         response_handler = NutClient.GenericGetCommandHandler(command)
         result = await self._execute(command, response_handler)
-        return result
+        return result[0]
 
-    async def get_status(self, ups) -> UpsStatus:
+    async def get_status(self, ups) -> List[UpsStatus]:
         result = await self.get("VAR", ups, "ups.status")
-        return UpsStatus(result[0].strip('"'))
+        try:
+            return [UpsStatus(item) for item in result.split(" ")]
+        except:
+            return []
